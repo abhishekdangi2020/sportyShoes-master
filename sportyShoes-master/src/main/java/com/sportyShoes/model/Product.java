2c13165abf3d102a3928035797451f6e3b31c70c
abhishekdangi2020 <dangiabhishek738@gmail.com>
2020-09-10 19:07:11 +0530

    First Commit

---
+++ sportyShoes-master/src/main/java/com/sportyShoes/model/Product.java
@@ -0,0 +1,34 @@
+package com.sportyShoes.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "productId", updatable = false, nullable = true)
+	private int productId;
+	private String productName;
+	private String productCategory;
+	private int productPrice;
+	
+	//@Lob
+	//@Column(columnDefinition = "CLOB")
+	private String productImage;
+	//private String avatar;
+}
